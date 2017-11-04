7aa54d57f68fc4b90ed1ae68ad00331c0ba9258e
threedaysdying@hotmail.com <isaque015@outlook.com.br>
2017-11-04 14:05:41 -0200

    Criando projeto django

--- Pmaster/Pmaster/urls.py
+++ Pmaster/Pmaster/urls.py
@@ -1,4 +1,6 @@
-"""projetoA URL Configuration
+
+
+"""Pmaster URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,7 +17,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from core.views import *
 
 urlpatterns = [
+    url(r'^$',index),
     url(r'^admin/', admin.site.urls),
 ]
