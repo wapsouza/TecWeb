936b45c7294cd5dc2952d5fd42013f533b8ab89f
isaque015@outlook.com.br <isaque015@outlook.com.br>
2017-11-10 16:02:51 -0200

    Arrumando script sql

--- Pmaster/scriptcomId.sql
+++ Pmaster/scriptcomId.sql
@@ -34,7 +34,7 @@ go
 create table Professor (
    id int IDENTITY(1,1) not null,
     ra int not null,
-    apelido varchar(30),
+    apelido varchar(30) not null,
     nome varchar (120) not null ,
     email varchar(80) not null,
     celular varchar(11),
@@ -46,11 +46,11 @@ go
 
 create table Aluno (
   	id int IDENTITY(1,1) not null,
-	ra int not null,
-	nome varchar (120) not null ,
-	email varchar (80) not null,
-	celular varchar(11),
-	sigla_curso char(2),
+	 ra int not null,
+	 nome varchar (120) not null ,
+	 email varchar (80) not null,
+	 celular varchar(11),
+	 sigla_curso char(2),
 
 	constraint pk_aluno primary key (id),
 	constraint uq_aluno unique (ra),
@@ -59,87 +59,69 @@ go
 
 create table GradeCurricular (
     id int IDENTITY(1,1) not null,
-    sigla_curso varchar(5) not null,
     ano smallint,
     semestre char(1) not null,
     id_curso int not null,
 
     constraint pk_gradecurricular primary key (id),
     constraint fk_idCurso foreign key (id_curso) references Curso(id),
-    constraint uq_gradecurricular unique (ano, semestre,sigla_curso),
+    constraint uq_gradecurricular unique (ano, semestre),
 );
 go
 
 create table Periodo (
-	id int IDENTITY(1,1) not null,
-    sigla_curso varchar (5),
-    ano_grade smallint,
-    semestre_grade char(1),
+	  id int IDENTITY(1,1) not null,
     numero tinyint,
-	id_gradecurricular int not null,
+	  id_gradecurricular int not null,
 
     constraint pk_periodo primary key (id),
-    constraint uq_periodo unique(sigla_curso, ano_grade, semestre_grade, numero),
+    constraint uq_periodo unique(numero),
     constraint fk_periodo foreign key (id_gradecurricular) references GradeCurricular(id),
 );
 go
 
 create table PeriodoDisciplina (
 	id int IDENTITY(1,1) not null,
-	sigla_curso varchar (5),
-    ano_grade smallint,
-	semestre_grade char(1),
-    numero_periodo tinyint,
-	nome_disciplina varchar (240),
 	id_periodo int not null,
+  id_disciplina int not null,
 
 	constraint pk_periododisciplina primary key (id),
-	constraint uq_periododisciplina unique (sigla_curso,ano_grade,semestre_grade,numero_periodo,nome_disciplina),
 	constraint fk_periododisciplina foreign key (id_periodo) references Periodo(id),
+  constraint fk_id_disciplina foreign key (id_disciplina) references Disciplina(id),
 );
 go
 
 create table DisciplinaOfertada (
 	id  int IDENTITY(1,1) not null,
-	nome_disciplina varchar(240) not null,
 	ano smallint,
 	semestre char(1),
 	id_disciplina int  not null,
 
 
 	constraint pk_iddisciplinaofertada primary key(id),
-	constraint uq_iddisciplinaofertada unique (nome_disciplina, ano, semestre),
+	constraint uq_iddisciplinaofertada unique (ano, semestre),
 	constraint fk_iddisciplinaofertada foreign key (id_disciplina) references Disciplina(id)
 );
 go
 
 create table Turma (
 	id int IDENTITY(1,1) not null,
-	nome_disciplina varchar(240),
-	ano_ofertado smallint,
-	semestre_ofertado char(1),
-	turno varchar(15) not null,
+	turno varchar(15) not null not null,
 	id_professor int not null,
 	id_disciplinaofertada int not null,
 
 	constraint pk_turma primary key(id),
 	constraint fk_id_professor foreign key (id_professor) references Professor(id),
 	constraint fk_id_disciplinaOfertada foreign key (id_disciplinaofertada) references DisciplinaOfertada(id),
-	constraint uq_turma unique (nome_disciplina,ano_ofertado,semestre_ofertado,id)
 );
 go
 
 create table CursoTurma (
 	id int IDENTITY(1,1) not null,
-	sigla_curso varchar(5),
-	nome_disciplina varchar(240),
-	ano_ofertado smallint,
-	semestre_ofertado char(1),
 	id_turma int not null,
 	id_curso int not null,
 
 	constraint pk_cursoturma primary key (id),
-	constraint uq_cursoturma unique (sigla_curso, nome_disciplina, ano_ofertado,semestre_ofertado,id_turma),
 	constraint fk_id_curso foreign key (id_curso) references Curso(id),
 	constraint fk_id_turma foreign key (id_turma) references Turma(id),
 );
