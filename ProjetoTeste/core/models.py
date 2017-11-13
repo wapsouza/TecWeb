44fcc9339bffb6891f40d10927f5e2b80bc7d998
Isaque015 <isaque015@outlook.com.br>
2017-11-13 20:22:24 -0200

    sa

--- ProjetoTeste/core/models.py
+++ ProjetoTeste/core/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, UserManager
-
+'''
 class Usuario(AbstractBaseUser):
 
     nome = models.CharField("Nome",max_length=100, blank=True)
     ativo = models.BooleanField("Ativo", default="True")
     senha = models.CharField("Senha")
-
+'''
 
 # Create your models here.
